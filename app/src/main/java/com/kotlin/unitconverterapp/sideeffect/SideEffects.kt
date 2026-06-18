b008fb736c77ba4e16715a975d0ca526806a3b60
Amit Bansode <amitb@creativegalileo.com>
2026-06-18 06:07:43 +0530

    snack bar host and snack bar host state using scaffold

--- app/src/main/java/com/kotlin/unitconverterapp/sideeffect/SideEffects.kt
+++ app/src/main/java/com/kotlin/unitconverterapp/sideeffect/SideEffects.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -18,17 +20,15 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
-import java.time.format.TextStyle
 
 
 @Composable
@@ -39,62 +39,67 @@ fun SideEffect(
     var total by remember { mutableStateOf(0.0) }
     var input by remember { mutableStateOf("") }
     var round by remember { mutableStateOf(1) }
-    val scope = rememberCoroutineScope()
+
     val snackbarHostState = remember { SnackbarHostState() }
 
-    LaunchedEffect(key1 = round, block = {
-        scope.launch { snackbarHostState.showSnackbar("Round $round") }
-    })
+    LaunchedEffect(key1 = round) {
+        snackbarHostState.showSnackbar("Round $round")
+    }
 
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(50.dp),
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Text(
-            modifier = modifier.fillMaxWidth(),
-            text = "Total is ${total.toString()}",
-            fontWeight = FontWeight.Bold,
-            fontSize = 30.sp,
-            color = Color.DarkGray
-        )
-
-        OutlinedTextField(
-            modifier = modifier.fillMaxWidth(),
-            placeholder = { Text("Enter value here") },
-            value = input,
-            onValueChange = {
-                input = it
-            },
-            textStyle = androidx.compose.ui.text.TextStyle(
-                color = Color.LightGray,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            ),
-            label = { Text(text = "New count") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number
-            )
-        )
-
-        Button(
-            modifier = modifier.fillMaxWidth(),
-            onClick = {
-                total += input.toDouble()
-                if (total > 300) {
-                    round += 1
-                    total = 0.0
-                    input = ""
-                }
-            }
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(50.dp),
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
             Text(
-                text = "Count",
+                modifier = modifier.fillMaxWidth(),
+                text = "Total is ${total.toString()}",
+                fontWeight = FontWeight.Bold,
                 fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
+                color = Color.DarkGray
             )
+
+            OutlinedTextField(
+                modifier = modifier.fillMaxWidth(),
+                placeholder = { Text("Enter value here") },
+                value = input,
+                onValueChange = {
+                    input = it
+                },
+                textStyle = TextStyle(
+                    color = Color.DarkGray,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                label = { Text(text = "New count") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+
+            Button(
+                modifier = modifier.fillMaxWidth(),
+                onClick = {
+                    val value = input.toDoubleOrNull() ?: 0.0
+                    total += value
+                    if (total > 300) {
+                        round += 1
+                        total = 0.0
+                        input = ""
+                    }
+                }
+            ) {
+                Text(
+                    text = "Count",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
 }
