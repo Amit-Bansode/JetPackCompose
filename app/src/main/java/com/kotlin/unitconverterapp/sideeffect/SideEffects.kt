f3bddf576e5fba2f5a8fd5a3db7f4d000f830433
Amit Bansode <amitb@creativegalileo.com>
2026-06-23 10:53:10 +0530

    Snack bar in compose

--- app/src/main/java/com/kotlin/unitconverterapp/sideeffect/SideEffects.kt
+++ app/src/main/java/com/kotlin/unitconverterapp/sideeffect/SideEffects.kt
@@ -1,8 +1,8 @@
 package com.kotlin.unitconverterapp.sideeffect
 
 import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,12 +10,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,72 +29,86 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun SideEffect(
-    modifier: Modifier = Modifier,
-    context: Context = LocalContext.current
+    modifier: Modifier = Modifier, context: Context = LocalContext.current
 ) {
     var total by remember { mutableStateOf(0.0) }
     var input by remember { mutableStateOf("") }
     var round by remember { mutableStateOf(1) }
+
+    val snackbarHostState = remember { SnackbarHostState() }
     //side effect. making toast is not in the scope of composition. wo its get called every time recomposition happens
     // this needs to be handled by LaunchedEffect
     LaunchedEffect(key1 = round) {
-        Toast.makeText(context, "Please start counting: Round$round", Toast.LENGTH_SHORT).show()
+        snackbarHostState.showSnackbar(
+            "Please start counting: Round$round",
+            "Dismiss",
+            duration = SnackbarDuration.Short
+        )
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp)
-            .padding(50.dp),
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Text(
-            modifier = modifier.fillMaxWidth(),
-            text = "Total is ${total.toString()}",
-            fontWeight = FontWeight.Bold,
-            fontSize = 30.sp,
-            color = Color.DarkGray
-        )
+    Scaffold(
+        snackbarHost = {
+            androidx.compose.material3.SnackbarHost(snackbarHostState)
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp)
+                    .padding(50.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    modifier = modifier.fillMaxWidth(),
+                    text = "Total is ${total.toString()}",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    color = Color.DarkGray
+                )
 
-        OutlinedTextField(
-            modifier = modifier.fillMaxWidth(),
-            placeholder = { Text("Enter value here") },
-            value = input,
-            onValueChange = {
-                input = it
-            },
-            textStyle = TextStyle(
-                color = Color.DarkGray,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            ),
-            label = { Text(text = "New count") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number
-            )
-        )
+                OutlinedTextField(
+                    modifier = modifier.fillMaxWidth(),
+                    placeholder = { Text("Enter value here") },
+                    value = input,
+                    onValueChange = {
+                        input = it
+                    },
+                    textStyle = TextStyle(
+                        color = Color.DarkGray, fontSize = 30.sp, fontWeight = FontWeight.Bold
+                    ),
+                    label = { Text(text = "New count") },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    )
+                )
 
-        Button(
-            modifier = modifier.fillMaxWidth(),
-            onClick = {
-                val value = input.toDoubleOrNull() ?: 0.0
-                total += value
-                if (total > 300) {
-                    input = ""
-                    total = 0.0
-                    round += 1
+                Button(
+                    modifier = modifier.fillMaxWidth(), onClick = {
+                        val value = input.toDoubleOrNull() ?: 0.0
+                        total += value
+                        if (total > 300) {
+                            input = ""
+                            total = 0.0
+                            round += 1
+                        }
+                    }) {
+                    Text(
+                        text = "Count", fontSize = 30.sp, fontWeight = FontWeight.Bold
+                    )
                 }
             }
-        ) {
-            Text(
-                text = "Count",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            )
         }
     }
+
+
 }
